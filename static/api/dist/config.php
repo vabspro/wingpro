20fa63d89aba49f94933fd6a68038f5a2b11ca75
uhorn <uwehornnet@users.noreply.github.com>
2023-11-29 20:46:46 +0100

    cuurent deployed version

--- static/api/dist/config.php
+++ static/api/dist/config.php
@@ -1,7 +1,7 @@
 <?php $config = array (
-  'api_token' => 'db03fa33c1e2ca35794adbb14aebb153',
-  'client_id' => 'f806517104bbc39a1aa569a35589489b',
-  'url' => 'https://test.vabs.pro/API/public',
+  'api_token' => '',
+  'client_id' => '',
+  'url' => '',
   'referrer' => '2',
   'dsgvo' => NULL,
   'agb' => NULL,
